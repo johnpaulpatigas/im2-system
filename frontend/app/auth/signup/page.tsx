950cab59b4e47fdd0c9c2d4471d650f1ee9b46ee
John Paul Patigas <johnpaulpatigas@gmail.com>
2025-09-20 20:09:05 +0800

    frontend: Dummy sign up page

---
+++ frontend/app/auth/signup/page.tsx
@@ -0,0 +1,101 @@
+// src/app/auth/signup/page.tsx
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign Up</CardTitle>
+        </CardHeader>
+
+        <CardContent className="space-y-4">
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="firstname">First Name</Label>
+              <Input id="firstname" type="text" placeholder="John" required />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="lastname">Last Name</Label>
+              <Input id="lastname" type="text" placeholder="Doe" required />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="email@email.com"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="confirmPassword">Confirm Password</Label>
+              <Input
+                id="confirmPassword"
+                type="password"
+                placeholder="••••••••"
+                required
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Sign Up
+            </Button>
+          </form>
+
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs">
+              <span className="bg-background text-muted-foreground px-2">
+                Or
+              </span>
+            </div>
+          </div>
+
+          <div className="space-y-4">
+            <Button variant="secondary" className="w-full">
+              <Image
+                src="/assets/google.svg"
+                alt="Google logo"
+                width={12}
+                height={12}
+              />
+              <span>Continue with Google</span>
+            </Button>
+
+            <p className="text-sm">
+              Already have an account?{" "}
+              <Link
+                href="/auth/signin"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
