3fece0df759be4a03e4cf6b554b7253e5ee7737f
John Paul Patigas <johnpaulpatigas@gmail.com>
2025-09-20 19:54:10 +0800

    frontend: Dummy sign in page

---
+++ frontend/app/auth/signin/page.tsx
@@ -0,0 +1,90 @@
+// src/app/auth/signin/page.tsx
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign In</CardTitle>
+        </CardHeader>
+
+        <CardContent className="space-y-4">
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="email@email.com"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                required
+              />
+            </div>
+
+            <div>
+              <Link
+                href="/auth/forgot-password"
+                className="text-sm text-blue-500 hover:text-blue-700"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+
+            <Button type="submit" className="w-full">
+              Sign In
+            </Button>
+          </form>
+
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs">
+              <span className="bg-background text-muted-foreground px-2">
+                Or
+              </span>
+            </div>
+          </div>
+
+          <div className="space-y-4">
+            <Button variant="secondary" className="w-full">
+              <Image
+                src="/assets/google.svg"
+                alt="Google logo"
+                width={12}
+                height={12}
+              />
+              <span>Continue with Google</span>
+            </Button>
+
+            <p className="text-sm">
+              Don&apos;t have an account?{" "}
+              <Link
+                href="/auth/signup"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
