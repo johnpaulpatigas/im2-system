95e6502173b227ef11486202d1a9d818f569500a
johnpaulpatigas <johnpaulpatigas@gmail.com>
2025-09-22 01:31:28 +0800

    backend: Add AppError utility and fix 404 catch-all route

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-app.all("*", (req, res, next) => {
+app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
